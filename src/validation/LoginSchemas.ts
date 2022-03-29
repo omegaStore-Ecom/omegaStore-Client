cc415413087f8777da3c37b3a9f52983f28853d6
Redouane Bouabana <rredouane342@gmail.com>
2022-03-29 10:06:39 +0000

    add input error handling

--- src/validation/LoginSchemas.ts
+++ src/validation/LoginSchemas.ts
@@ -1,7 +1,7 @@
 import * as Yup from 'yup';
 export const AdminLogin = Yup.object().shape({
     password: Yup.string()
-      .min(2, 'Too Short!')
+      .min(5, 'Too Short!')
       .max(50, 'Too Long!')
       .required('Required'),
     email: Yup.string().email('Invalid email').required('Required'),
