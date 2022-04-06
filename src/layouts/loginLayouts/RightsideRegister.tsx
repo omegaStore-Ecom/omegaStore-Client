972e532354e8aa9e0ea29dd10a0f23d25db70804
Redouane Bouabana <rredouane342@gmail.com>
2022-04-06 10:17:09 +0000

    refactor seller login | register

--- src/layouts/loginLayouts/RightsideRegister.tsx
+++ src/layouts/loginLayouts/RightsideRegister.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Image from 'next/image';
 import { FcGoogle } from 'react-icons/fc';
 import { FaFacebookF } from 'react-icons/fa';
+import SellerRegisterForm from './../../components/Forms/Seller/SellerRegisterForm/index';
 
 
 const RightsideRegister: React.FC = () => {
@@ -26,74 +27,7 @@ const RightsideRegister: React.FC = () => {
             </button>
           </div>
           <p className="text-gray-100">or use email your account</p>
-          <form action="" className="mx-auto w-full px-4 sm:w-2/3 lg:px-0">
-            <div className="pb-2 pt-4">
-              <input
-                className="block w-full rounded-full bg-black p-4 text-lg"
-                type="text"
-                name="firstname"
-                placeholder="First Name"
-              />
-            </div>
-
-            <div className="pb-2 pt-4">
-              <input
-                className="block w-full rounded-full bg-black p-4 text-lg"
-                type="text"
-                name="lastname"
-                placeholder="Last Name"
-              />
-            </div>
-
-            <div className="pb-2 pt-4">
-              <input
-                type="email"
-                name="email"
-                id="email"
-                placeholder="Email"
-                className="block w-full rounded-full bg-black p-4 text-lg"
-              />
-            </div>
-
-            <div className="pb-2 pt-4">
-              <input
-                className="block w-full rounded-full bg-black p-4 text-lg"
-                type="password"
-                name="password"
-                placeholder="Password"
-              />
-            </div>
-
-            <div className="pb-2 pt-4">
-              <input
-                className="block w-full rounded-full bg-black p-4 text-lg"
-                type="number"
-                name="phone"
-                placeholder="Phone"
-              />
-            </div>
-
-            <div className="pb-2 pt-4">
-              <label
-                htmlFor="file"
-                className="block w-full rounded-full bg-black p-4 text-lg text-gray-400"
-              >
-                Import CIN
-              </label>
-              <input
-                className="hidden w-full rounded-full bg-black p-4 text-lg"
-                type="file"
-                name="file"
-                id="file"
-                placeholder="file"
-              />
-            </div>
-            <div className="px-4 pb-2 pt-4">
-              <button className="block w-full rounded-full bg-indigo-500 p-4 text-lg uppercase hover:bg-indigo-600 focus:outline-none">
-                sign in
-              </button>
-            </div>
-          </form>
+          <SellerRegisterForm/>
         </div>
       </div>
     );
