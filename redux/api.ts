83af8a83a3c5be52998569c04ed4064e13386e79
saiess <saidessalami0@gmail.com>
2022-04-26 12:28:57 +0000

    server deploy link changed

--- redux/api.ts
+++ redux/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@rtk-incubator/rtk-query/react";
 import { AppState } from "./store";
 
 export const baseQuery = fetchBaseQuery({
-  baseUrl: "https://cryptomr.herokuapp.com/",
+  baseUrl: "https://cryptom-server.herokuapp.com/",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as AppState).currentUser.token;
     if (token) {
